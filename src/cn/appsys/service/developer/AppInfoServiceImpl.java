d559adc8fe73f03efaa1f124bea14f6ba9347b9e
YOGA <YOGA@254.194.9>
2018-08-26 15:11:51 +0800

    完成功能-查询app集合

--- src/cn/appsys/service/developer/AppInfoServiceImpl.java
+++ src/cn/appsys/service/developer/AppInfoServiceImpl.java
@@ -3,6 +3,8 @@ package cn.appsys.service.developer;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
 import cn.appsys.dao.appinfo.AppInfoMapper;
@@ -10,6 +12,7 @@ import cn.appsys.pojo.AppInfo;
 
 @Service
 public class AppInfoServiceImpl implements AppInfoService {
+	@Resource
 	private AppInfoMapper appInfoMapper;
 
 	@Override
@@ -17,18 +20,23 @@ public class AppInfoServiceImpl implements AppInfoService {
 			Integer queryCategoryLevel2, Integer queryCategoryLevel3, Integer queryFlatformId, Integer devId,
 			Integer currentPageNo, Integer pageSize) {
 		// TODO Auto-generated method stub
-		List<AppInfo> list =new ArrayList<AppInfo>();
+		List<AppInfo> list = new ArrayList<AppInfo>();
 		try {
-			list = appInfoMapper.getAppInfoList(querySoftwareName, queryStatus, queryCategoryLevel1, queryCategoryLevel2,
-					queryCategoryLevel3, queryFlatformId, devId, currentPageNo, pageSize);
+			list = appInfoMapper.getAppInfoList(querySoftwareName, queryStatus, queryCategoryLevel1,
+					queryCategoryLevel2, queryCategoryLevel3, queryFlatformId, devId, currentPageNo, pageSize);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		for(AppInfo a : list){
-			System.out.println(a.getAPKName()+"=================================================");
-		}
 		return list;
+	}
+
+	@Override
+	public int getAppInfoCount(String querySoftwareName, Integer queryStatus, Integer queryCategoryLevel1,
+			Integer queryCategoryLevel2, Integer queryCategoryLevel3, Integer queryFlatformId, Integer devId) {
+		return appInfoMapper.getAppInfoCount(querySoftwareName, queryStatus, queryCategoryLevel1, queryCategoryLevel2,
+				queryCategoryLevel3, queryFlatformId, devId);
+
 	}
 
 }
